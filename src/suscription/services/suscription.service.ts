cb5ebfde9f015e0138703956cd7e412a1691e3d3
Sonikuc <soniikkuc@hotmail.com>
2022-10-30 22:54:36 -0400

    solucionada validacion de inscripcion a curso no publicado

--- src/suscription/services/suscription.service.ts
+++ src/suscription/services/suscription.service.ts
@@ -15,15 +15,23 @@ export class SuscriptionService {
 
     async createSuscription(id_student:number, id_course: number){        
         const newSuscription: Suscription = this.suscriptionrepo.create();
+
         const newStudent: Student = await this.studentrepo.findOne({where: {id_Student: id_student}});
         const newCourse: Courses = await this.coursesrepo.findOne({where: {id: id_course}});
+        const qb = await this.suscriptionrepo.createQueryBuilder("suscription").where("suscription.course_id = :id_course and suscription.student_id =:id_student").setParameters({id_course,id_student}).getOne();
+
+
+        if (qb != null){
+            return ('Ya esta inscrito en este curso')
+        }
+
         if (newStudent.state == "Blocked"){
             return "No se puede suscribir, esta bloqueado"
         }
 
         if(newCourse.state == "published"){
             newSuscription.student = newStudent;
-        newSuscription.course = newCourse;
+            newSuscription.course = newCourse;
 
         return (this.suscriptionrepo.save(newSuscription))
         }
